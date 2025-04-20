ea67f8f5969a98562b06d600caf7301e33902cfe
DaveedGangi <daveedgangi@gmail.com>
2025-04-20 14:14:49 +0530

    firstcommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,145 @@
-import Image from "next/image";
+import {useState,useEffect} from "react";
+
+
 import { Geist, Geist_Mono } from "next/font/google";
+import ClothingItem from "@/components/ClothingItem";
+import Canvas from "@/components/Canvas";
+import Cart from "@/components/Cart";
+import Savedoutfits from "@/components/savedOutfits";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
+
+  const[outfits,setOutfits]=useState<any[]>([])
+
+  const[addCartOutfits,setAddCartOutfits]=useState<any[][]>([])
+  const[savedOutfits,setSavedOutfits]=useState<any[][]>([])
+
+  useEffect(()=>{
+
+    const outfits=JSON.parse(localStorage.getItem("savedOutfits")||"[]")
+    console.log("loadded outfits",outfits);
+    setSavedOutfits(outfits);
+
+},[])
+
+
+
+  const clothingItems=[
+    {
+      imageSrc:"/images/shirtimage1.png",
+      label:"Shirt",
+      position:"middle"
+    },
+    {
+      imageSrc:"/images/pantimage1.jpeg",
+      label:"Pant",
+      position:"bottom"
+    },
+    {
+      imageSrc:"/images/shoesimage1.jpg",
+      label:"Shoes",
+      position:"veryBottom"
+    },
+    {
+      imageSrc:"/images/capimage1.jpeg",
+      label:"Cap",
+      position:"top"
+    },
+    {
+      imageSrc:"/images/jeansimage1.jpeg",
+      label:"Jeans",
+      position:"bottom"
+    },
+    {
+      imageSrc:"/images/tshirtimage1.jpeg",
+      label:"T-shirt",
+      position:"middle"
+    }
+  ]
+
+const handleDragStart=(e:React.DragEvent,item:any)=>{
+                e.dataTransfer.setData("item",JSON.stringify(item))
+}
+
+const handleAddCartOutfit=(newOutfit:any[])=>{
+          setOutfits((prev)=>[...prev,newOutfit])
+
+}
+
+const handleRemoveOutfit=(outfitIndex:number,itemIndex:number)=>{
+  const updatedOutfits=[...outfits]
+  updatedOutfits[outfitIndex]=updatedOutfits[outfitIndex].filter((_,indexData)=>indexData!==itemIndex)
+  
+  const cleanedOutfits=updatedOutfits.filter((each)=>each.length>0);
+  setOutfits(cleanedOutfits)
+}
+
+const handleClearOutfits=()=>{
+  localStorage.removeItem("savedOutfits");
+  setSavedOutfits([]);
+}
+
+
+const handleSaveToLocal=(items:any[])=>{
+  if(items.length===0){
+      return
+  }
+  const existingOutfits=JSON.parse(localStorage.getItem("savedOutfits")||"[]");
+  const updatedOutfitsData=[...existingOutfits,items]
+  localStorage.setItem("savedOutfits",JSON.stringify(updatedOutfitsData))
+  setSavedOutfits(updatedOutfitsData);
+  setOutfits([])
+
+
+}
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <h1 className="text-center font-bold m-3 text-amber-800 text-4xl  text-shadow-amber-200 ">Outfit Builder </h1>
+      <div className="flex  mt-12 text-center flex-row justify-around">
+      <h2 className="font-extralight text-3xl ">Clothing Items</h2>
+      <h2 className="font-extralight  mb-6 text-3xl">Canvas</h2>
+
+      </div>
+    <div className="flex flex-row justify-center  p-8">
+   
+      <div className="w-full md:w-1/4 flex  flex-col  items-center  overflow-auto h-140">
+    
+        {
+          clothingItems.map((item,index)=>(
+            <ClothingItem 
+            key={index} 
+            imageSrc={item.imageSrc}
+            label={item.label}
+            onDragStart={(e)=>handleDragStart(e,item,)}/>
+          ))
+        }
+      </div>
+
+      <div className="w-full md:w-1/2 mb-3 ">
+       
+        <Canvas onAddCartOutfit={handleAddCartOutfit} onSaveToLocal={handleSaveToLocal}/>
+      </div>
+
+    
+     
+    </div>
+
+<hr/>
+<div className="w-full flex flex-col md:flex-row justify-center md:justify-around items-center">
+    <div className="md:w-1/4">
+        <Cart outfits={outfits} removeItem={handleRemoveOutfit}/>
+      </div>
+
+    <div className="md:w-1/4 mt-5 mb-5">
+      
+      <Savedoutfits  savedOutfits={savedOutfits} onClear={handleClearOutfits}/>
+    </div>
+</div>
+
+
     </div>
   );
 }
